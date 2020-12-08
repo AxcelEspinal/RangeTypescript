d9c348841857c623f8573e86e46938958cdb97cb
Axcel Espinal <hyrulelink8zelda@gmail.com>
2020-12-07 22:11:05 -0400

    initial commit

---
+++ src/range.ts
@@ -0,0 +1,15 @@
+export default class Range {
+
+    public startpoint: number;
+    public endpoint: number;
+
+    constructor(startpoint, endpoint) {
+        this.startpoint = startpoint;
+        this.endpoint = endpoint;
+    }
+
+    public GetAllPoints(): string {
+        return
+
+    }
+}
---
+++ test/range.ts
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+
+import Range from "../src/range";
+
+describe('Testing Range', () => {
+
+    describe('Range = [2, 6)  GetAllPoints = {2, 3, 4, 5]'), () => {
+        it('should return {2, 3, 4, 5] '), () => {
+            let ran = new Range(2, 6);
+            expect(ran.GetAllPoints()).to.equal(" {2, 3, 4, 5]")
+        }
+    }
+
+})
