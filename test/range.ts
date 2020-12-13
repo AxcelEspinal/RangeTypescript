235c8540bd938dd4d79439bfb8b87ca40c07b4e9
Axcel Espinal <hyrulelink8zelda@gmail.com>
2020-12-13 10:01:28 -0400

    Borrar todo

--- test/range.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai";
-
-const methods = require("../src/range");
-
-describe("Range (2,10)  Endpoints ShouldBe{3,9}", function () {
-    it("Should return: {3,9}", function () {
-        expect(methods.EndPoints("(2,10)")).to.be.eqls([3, 9])
-    });
-});
